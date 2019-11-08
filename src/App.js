165a29ffbfa019161e2312de5f9e3c11be097c01
raberin <rmvaberin@gmail.com>
2019-11-08 13:17:57 -0800

    Module 2 - Tools

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt me!"),
     React.createElement(Pet, { name: "Oreo", animal: "Dog", breed: "Husky" }),
-    React.createElement(Pet, { name: "Poop", animal: "Dog", breed: "Lab" }),
+    React.createElement(Pet, { name: "Poop", animal: "Dog", breed: "Labdor" }),
     React.createElement(Pet, { name: "Toot", animal: "Dog", breed: "Maltese" })
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
