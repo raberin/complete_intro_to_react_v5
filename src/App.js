decde45d3881fc9cd26e391a725b058652ffa028
raberin <rmvaberin@gmail.com>
2019-11-08 14:26:56 -0800

    Module 4 - Hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt me!"),
-  //   React.createElement(Pet, { name: "Oreo", animal: "Dog", breed: "Husky" }),
-  //   React.createElement(Pet, { name: "Poop", animal: "Dog", breed: "Labdor" }),
-  //   React.createElement(Pet, { name: "Toot", animal: "Dog", breed: "Maltese" })
-  // ]);
-
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Oreo" animal="Dog" breed="Husky" />
-      <Pet name="Poop" animal="Dog" breed="Lab" />
-      <Pet name="Toote" animal="Dog" breed="Maltese" />
+      <SearchParams />
     </div>
   );
 };
