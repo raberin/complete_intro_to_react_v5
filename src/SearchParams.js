e43ac1bf64803016ea31eb25d97752c1202ab2b6
raberin <rmvaberin@gmail.com>
2020-05-04 13:38:32 -0700

    Async API Requests - Done

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,48 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
+
+  // Replaces componentDidMount lifecycle aka... API calls
+  // Used for asynchronous calls
+  useEffect(() => {
+    // Empty out breeds state
+    setBreeds([]);
+    setBreed("");
+
+    // Destructure breeds to access objects inside it
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      // Grab the name from each dog object and return the name
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
